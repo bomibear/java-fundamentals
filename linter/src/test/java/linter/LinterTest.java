b731b24bc1054f94d9cb3af0fed7502a704920c6
bomibear <bomibearly@gmail.com>
2019-08-08 14:09:50 -0700

    add test to check if file is empty

--- linter/src/test/java/linter/LinterTest.java
+++ linter/src/test/java/linter/LinterTest.java
@@ -2,16 +2,48 @@ package linter;
 
 import org.junit.Test;
 
+import java.io.*;
+
+
 import static org.junit.Assert.*;
 
 public class LinterTest {
     @Test
-    public void canCheckforErrors(){
+    public void canCheckAnEmptyFile(){
+        String filePath = "./src/main/resources/gates.js";
+
         Linter newLinter = new Linter();
-        newLinter.LinterCheck();
-        String expected = "Hello";
-        String actual = "Hello";
-        assertEquals(expected, actual);
+        String actual = newLinter.LinterCheck(filePath);
+        String expected = "Empty file";
+        assertTrue(actual.equals(expected));
+
     }
+//    @Test
+//    public void canCheckIfEmpty(){
+//        String filePath = "./src/main/resources/empty.js";
+//        try {
+//            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+//            String line = reader.readLine();
+//            if (reader.readLine() == null) {
+//                System.out.println("No errors, and file empty");
+//            }
+//        } catch (IOException e) {
+//            System.out.println("the file was not found");
+//        }
+//
+//        Linter newLinter = new Linter();
+//        newLinter.LinterCheck();
+//        String expected = "Hello";
+//        String actual = "Hello";
+//        assertEquals(expected, actual);
+//    }
+//    @Test
+//    public void canCheckIfEmpty(){
+//        Linter newLinter = new Linter();
+//        newLinter.LinterCheck();
+//        String expected = "Hello";
+//        String actual = "Hello";
+//        assertEquals(expected, actual);
+//    }
 
 }
