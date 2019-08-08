4e017b8a7ca598323fd258de2ca5aeffe0acbcec
bomibear <bomibearly@gmail.com>
2019-08-08 15:12:57 -0700

    finish all tests

--- linter/src/test/java/linter/LinterTest.java
+++ linter/src/test/java/linter/LinterTest.java
@@ -38,24 +38,27 @@ public class LinterTest {
         assertTrue(actual.length() == 27);
     }
 
-//    @Test
-//    public void canCheckForFewErrors(){
-//        String filePath = "./src/main/resources/fewError.js";
-//
-//        Linter newLinter = new Linter();
-//        String actual = newLinter.LinterCheck(filePath);
-//        //each message line is 27 characters long, and 1 error message is 27 characters long
-//        //3 error messages in this ex, so 27 * 3
-//        assertTrue(actual.length() == 81);
-//    }
+    @Test
+    public void canCheckForFewErrors(){
+        String filePath = "./src/main/resources/fewErrors.js";
+
+        Linter newLinter = new Linter();
+        String actual = newLinter.LinterCheck(filePath);
+        //each message line is 27 characters long, and 1 error message is 27 characters long
+        //3 errors, so 27 * 3
+        assertTrue(actual.length() == 81);
+    }
 
 
-//    @Test
-//    public void canCheckForManyErrors(){
-//        String filePath = "./src/main/resources/manyError.js";
-//
-//        Linter newLinter = new Linter();
-//        String actual = newLinter.LinterCheck(filePath);
-//        assertTrue(actual.length() == 1);
-//    }
+    @Test
+    public void canCheckForManyErrors(){
+        String filePath = "./src/main/resources/manyErrors.js";
+
+        Linter newLinter = new Linter();
+        String actual = newLinter.LinterCheck(filePath);
+        //each message line is 27 characters long, and 1 error message is 27 characters long
+        //since the numbers after the line extends the msg length, errors are found on lines with 2 numbers
+        //6 errors, so 28 * 10
+        assertTrue(actual.length() == 280);
+    }
 }
