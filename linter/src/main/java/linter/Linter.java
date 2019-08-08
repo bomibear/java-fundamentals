dadc74ff385f4ac9d59b99f0f788681504aaa094
bomibear <bomibearly@gmail.com>
2019-08-08 13:22:14 -0700

    file is read in properly

--- linter/src/main/java/linter/Linter.java
+++ linter/src/main/java/linter/Linter.java
@@ -1,8 +1,29 @@
 package linter;
 
-public class Linter {
-    public String LinterCheck(){
+import java.io.*;
+import java.util.Scanner;
 
+public class Linter {
+    public static void main(String[] args) {
+        LinterCheck();
+    }
+
+    public static String LinterCheck(){
+        String filePath = "./src/main/resources/gates.js";
+        try {
+//            Scanner s = new Scanner(new File(filePath));
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line = reader.readLine();
+            while (line != null) {
+//                System.out.println(line);
+                
+                line = reader.readLine();
+
+            }
+            System.out.println("end of file");
+        } catch (IOException e) {
+            System.out.println("the file was not found");
+        }
         return "Hello";
     }
 }
